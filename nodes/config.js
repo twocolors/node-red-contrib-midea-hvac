61be37a802dcef04f424c8d15feb91ca664108f8
D <dmitry@yashin.su>
2022-05-18 23:29:47 +0300

    support discover

--- nodes/config.js
+++ nodes/config.js
@@ -1,38 +1,48 @@
-'use strict';
-
-const MideaCloud = require('../lib/midea-cloud');
-
 module.exports = function (RED) {
-  class MideaAccount {
-    constructor(config) {
-      RED.nodes.createNode(this, config);
+  "use strict";
 
-      let node = this;
-      node.config = config;
+  const discover = require("../lib/discover");
 
-      if (node.credentials.username && node.credentials.password) {
-        node.cloud = new MideaCloud({uid: node.credentials.username, password: node.credentials.password});
-      }
-    }
+  // class MideaAccount {
+  //   constructor(config) {
+  //     RED.nodes.createNode(this, config);
+
+  //     let node = this;
+  //     node.config = config;
+
+  //     if (node.credentials.username && node.credentials.password) {
+  //       node.cloud = new MideaCloud({uid: node.credentials.username, password: node.credentials.password});
+  //     }
+  //   }
+  // }
+
+  function HVACConfig(n) {
+    RED.nodes.createNode(this, n);
+
+    let node = this;
   }
 
-  RED.nodes.registerType("midea-hvac-config", MideaAccount, {
+  RED.nodes.registerType("midea-hvac-config", HVACConfig, {
     credentials: {
       username: { type: "text" },
-      password: { type: "password" }
-    }
+      password: { type: "password" },
+      id: { type: "text" },
+      key: { type: "text" },
+      token: { type: "text" },
+    },
   });
 
-  RED.httpAdmin.get('/midea/cloud/devices', (req, res) => {
-    let controller = RED.nodes.getNode(req.query.controller);
-    if (controller) {
-      controller.cloud.devices().then(response => {
-        res.json(response);
-      }).catch(error => {
-        res.json({ error: error.message });
-      });
-    } else {
-      res.status(404).end();
+  RED.httpAdmin.get("/midea-hvac/discover", async (req, res) => {
+    let username = req.query.username;
+    let password = req.query.password;
+    let devices  = [];
+
+    try {
+      devices  = await discover(username, password);
+    } catch (error) {
+      return res.json({ error: error });
     }
+
+    return res.json(devices);
   });
-}
+};
